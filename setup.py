7ef4438e8c34917fdf6ed7feae399c324f3efeb0
Jiovani Ledesma <43224656+JiovaniLedesma@users.noreply.github.com>
2019-06-08 22:27:48 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+# Current status: pre-alpha
+
+setup(name='learning_NeuralNetworks',
+      version='0.1.0',
+      description='Python library for learning about Neural Networks',
+      author='Jiovani Ledesma',
+      author_email='jiovaniledesmaa@gmail.com',
+      license = "MIT",
+      keywords=["AI","neural networks", "machine learning", "deep learning"],
+      url='https://github.com/JiovaniLedesma/Learning_NeuralNetworks',
+      packages=['learning_NeuralNetworks'],
+      classifiers=[
+      "Development Status :: 2 - Pre-Alpha",
+      "Intended Audience :: Education",
+      "Intended Audience :: Developers",
+      "License :: OSI Approved :: MIT License",
+      "Operating System :: OS Independent",
+      "Programming Language :: Python",
+      "Programming Language :: Python :: 3.6",
+      "Programming Language :: Python :: Implementation :: CPython",
+      ]
+      )
